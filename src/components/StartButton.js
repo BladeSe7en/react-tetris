bcaa96d53d17fdd533953b394ebdb30d351b58ae
BrianC <seahorse8789@gmail.com>
2020-10-04 20:51:30 -0700

    finished tetris game

---
+++ src/components/StartButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledStartButton } from './styles/StyledStartButton';
+
+const StartButton = ({ callBack }) => (
+    <StyledStartButton onClick = {callBack}> start game </StyledStartButton>
+)
+
+export default StartButton;
